e6b5664f98826fe8799460a93edf6ab08f54df66
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-07-08 17:31:04 +0200

    Include some Danish

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Testing Llama3 model for extraction of relevant words
 # Import libraries
 import os
 import pandas as pd
-from scripts.llama_model import model
+from scripts.llama_model import model, model_danish
 from scripts.gen_dataframe import gen_dataframe
 
 # Define the keyword
@@ -13,7 +13,7 @@ KEYWORD = input("Enter the word that you are interested in: ")
 
 def main():
     # Condition statemet
-    if os.path.exists("dataset_notes/clin_note_df.tsv"):
+    if os.path.exists("dataset_notes/clin_note_df.tsv") and os.path.exists("dataset_notes/clin_note_danish_df.tsv"):
         print("File exists")
     else:
         print("Generating a Dataframe with clinical notes")
@@ -22,11 +22,13 @@ def main():
 
     # Parse the dataset
     dataframe = pd.read_csv("dataset_notes/clin_note_df.tsv", sep="\t", dtype=object)
-
-    # Response of the model    
+    dataframe_danish = pd.read_csv("dataset_notes/clin_note_danish_df.tsv", sep="\t", dtype=object)
+    # Response of the model (English)    
     response = model(df=dataframe, keyword=KEYWORD)
-    print(response)
-
+    print(response, "\n\n")
+    # Respond of the model (Dansish)
+    response_danish = model_danish(df_da=dataframe_danish, keyword=KEYWORD)
+    print(response_danish)
 
 if __name__ == "__main__":
     main()
