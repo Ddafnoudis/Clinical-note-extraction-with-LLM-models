ac7f12dbf806198fb8e8964e589d915e364268e7
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-07-10 16:03:55 +0200

    Part of the code is commented. Needs reconstruction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """
-Testing Llama3 model for extraction of relevant words
+Testing Llama3 model for extraction of relevant words on
+clinical notes.
+
+Purpose: To check if the model can understand both languages and give same examples. 
 """
 # Import libraries
 import os
@@ -9,7 +12,7 @@ from scripts.gen_dataframe import gen_dataframe
 
 # Define the keyword
 KEYWORD = input("Enter the word that you are interested in: ")
-
+BATCH_SIZE = 20
 
 def main():
     # Condition statemet
@@ -20,15 +23,16 @@ def main():
         # Generate the DataFrame 
         gen_dataframe()
 
-    # Parse the dataset
+    # Parse the datasets
     dataframe = pd.read_csv("dataset_notes/clin_note_df.tsv", sep="\t", dtype=object)
     dataframe_danish = pd.read_csv("dataset_notes/clin_note_danish_df.tsv", sep="\t", dtype=object)
+
     # Response of the model (English)    
-    response = model(df=dataframe, keyword=KEYWORD)
+    response = model(df=dataframe, keyword=KEYWORD, batch_size=BATCH_SIZE)
     print(response, "\n\n")
     # Respond of the model (Dansish)
-    response_danish = model_danish(df_da=dataframe_danish, keyword=KEYWORD)
-    print(response_danish)
+    # response_danish = model_danish(df_da=dataframe_danish, keyword=KEYWORD, batch_size=BATCH_SIZE)
+    # print(response_danish)
 
 if __name__ == "__main__":
     main()
