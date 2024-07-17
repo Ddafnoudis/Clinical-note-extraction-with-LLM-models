c22b799fd57dde4e6642baf77be7606de8f2919c
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2024-07-17 11:32:18 +0200

    Updated

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from scripts.parse_data import parse_data
 from scripts.llama_test import llama_test
 from scripts.llama_model import model, model_danish
-# from scripts.llama_train_in_danish import train_llama3
+from scripts.llama_train_in_danish import train_llama3
 
 # Define keyword and paths
 # KEYWORD = input("Enter the word that you are interested in: ")
@@ -17,6 +17,9 @@ DATA_FOLDER = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_n
 DF_ENG = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_notes/clin_note_df.tsv").expanduser()
 DF_DK = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_notes/clin_note_danish_df.tsv").expanduser()
 
+# Set the batch size and number of epochs for training
+BATCH_SIZE = 16
+NUM_EPOCHS = 5
 
 def main():
     # Condition statement
@@ -27,17 +30,18 @@ def main():
 
 
     # Train the model
-    llama_test(df_dk=df_dk, llm_model=LLM_MODEL)
+    llama_test(df_dk=df_dk, llm_model=LLM_MODEL, batch_size=BATCH_SIZE, num_epochs=NUM_EPOCHS)
 
     # Train the Llama3 model 
-    # train_llama3(token_id=MY_TOKEN, llm_model=LLM_MODEL)
+    attempt_response = train_llama3(df_dk=df_dk)
+    print(attempt_response);exit()
 
-    # # Response of the model (English)    
-    # response = model(df_eng=df_eng, keyword=KEYWORD)
-    # print(response, "\n\n")
-    # # Respond of the model (Dansish)
-    # response_danish = model_danish(df_dk=df_dk, keyword=KEYWORD)
-    # print(response_danish)
+    # Response of the model (English)    
+    response = model(df_eng=df_eng, keyword=KEYWORD)
+    print(response, "\n\n")
+    # Respond of the model (Dansish)
+    response_danish = model_danish(df_dk=df_dk, keyword=KEYWORD)
+    print(response_danish)
 
 
 if __name__ == "__main__":
