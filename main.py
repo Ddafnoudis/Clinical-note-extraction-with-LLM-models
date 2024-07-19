b7d0fd03441baf8e0e3cc51c175e02d626834ac6
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-07-19 16:07:46 +0200

    Minor Updates

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ from scripts.llama_train_in_danish import train_llama3
 
 # Define keyword and paths
 # KEYWORD = input("Enter the word that you are interested in: ")
-MY_TOKEN = "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" #input("Provide your token: ")
-LLM_MODEL = "LLama3_instruct/Meta-Llama-3-8B-Instruct"
+LLM_MODEL = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/AI-Sweden-Models/Llama-3-8B").expanduser()
+MODEL_PATH = f"{LLM_MODEL}/model-00004-of-00004.safetensors"
+TOKENIZER_PATH = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/AI-Sweden-Models/Llama-3-8B/tokenizer.json").expanduser()
+# Fine-tuned model name
+NEW_MODEL=  Path("~/Desktop/Clinical-note-extraction-with-LLM-models/AI-Sweden-Models/Llama-3-8B/llama-3-8b-danish")
 DATA_FOLDER = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_notes").expanduser()
 DF_ENG = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_notes/clin_note_df.tsv").expanduser()
 DF_DK = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_notes/clin_note_danish_df.tsv").expanduser()
 
 # Set the batch size and number of epochs for training
-BATCH_SIZE = 16
-NUM_EPOCHS = 5
+BATCH_SIZE = 1
+NUM_EPOCHS = 1
 
 def main():
     # Condition statement
@@ -28,20 +31,20 @@ def main():
     df_eng = pd.read_csv(DF_ENG, sep="\t", dtype=object)
     df_dk = pd.read_csv(DF_DK, sep="\t", dtype=object)
 
-
+    
     # Train the model
-    llama_test(df_dk=df_dk, llm_model=LLM_MODEL, batch_size=BATCH_SIZE, num_epochs=NUM_EPOCHS)
+    llama_test(df_dk=df_dk, llm_model=LLM_MODEL, model_path=MODEL_PATH, tokenizer_path=TOKENIZER_PATH, batch_size=BATCH_SIZE, new_model=NEW_MODEL, num_epochs=NUM_EPOCHS)
 
     # Train the Llama3 model 
-    attempt_response = train_llama3(df_dk=df_dk)
-    print(attempt_response);exit()
+    # attempt_response = train_llama3(df_dk=df_dk)
+    # print(attempt_response);exit()
 
     # Response of the model (English)    
-    response = model(df_eng=df_eng, keyword=KEYWORD)
-    print(response, "\n\n")
-    # Respond of the model (Dansish)
-    response_danish = model_danish(df_dk=df_dk, keyword=KEYWORD)
-    print(response_danish)
+    # response = model(df_eng=df_eng, keyword=KEYWORD)
+    # print(response, "\n\n")
+    # # Respond of the model (Dansish)
+    # response_danish = model_danish(df_dk=df_dk, keyword=KEYWORD)
+    # print(response_danish)
 
 
 if __name__ == "__main__":
