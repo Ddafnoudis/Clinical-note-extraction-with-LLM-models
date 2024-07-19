ed2ba0a84f5bba4af54b473125c1a020e87c9770
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-07-19 16:08:56 +0200

    Cuda error(memory.. allocated 64.0 MiB)

--- scripts/llama_test.py
+++ scripts/llama_test.py
@@ -1,107 +1,131 @@
 """
 Training an Large Language Model (LLM) in Danish clinical text data
 """
-import os
 import torch
-from transformers import LlamaForCausalLM, AutoTokenizer, TrainingArguments
+torch.cuda.empty_cache()
+import gc
+gc.collect()
+from pathlib import Path
+from datasets import Dataset
+from torch.utils.data import DataLoader
+import torch
+from transformers import (
+    AutoTokenizer,
+    TrainingArguments,
+    AutoModelForCausalLM,
+    )
+
+
+from trl import SFTTrainer
 from tqdm import tqdm
 from typing import Dict
 from pandas import DataFrame
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
 
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-def llama_test(df_dk: DataFrame, llm_model: str, batch_size: int, num_epochs: int):
+def llama_test(df_dk: DataFrame, llm_model: Path, model_path: Path, tokenizer_path: Path, batch_size: int, new_model: str, num_epochs: int):
     """
     Train the Llama3 model in danish clinical text data
     """
-    # Preprocess the text data
-    tokenizer = AutoTokenizer.from_pretrained(llm_model, padding=True,
-                                              padding_side="right", pad_token="[PAD]",
-                                              add_eos_token=True, add_bos_token=True)
-    tokenizer.pad_token = tokenizer.eos_token
+    device = torch.device("cpu")
+    # Load base model as set it to the current device
+    model = AutoModelForCausalLM.from_pretrained(llm_model)
+    model = model.to(device)
+
+    # don't use the cache
+    model.config.use_cache = False
+
+    # Define the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(llm_model)
+    # tokenizer.pad_token = tokenizer.eos_token
 
     # Input_ids and attention_mask
-    def preprocess_text(text: str) -> Dict[torch.Tensor, torch.Tensor]:
+    def preprocess_text(text: str) -> str: #Dict[torch.Tensor, torch.Tensor]:
         """
         Tokenize the words of the sentence and encode them
         """
-        encoded_text = tokenizer.encode_plus(text, max_length=512,
-                                             padding="max_length", truncation=True,
-                                             return_attention_mask=True, return_tensors="pt")
-        return {
-            "input_ids": encoded_text["input_ids"].squeeze(),
-            "attention_mask": encoded_text["attention_mask"].squeeze()
-        }
+        # encoded_text = tokenizer.encode_plus(text, max_length=512,
+        #                                      padding="max_length", truncation=True,
+        #                                      return_attention_mask=True, return_tensors="pt")
+        # return {
+        #     "input_ids": encoded_text["input_ids"].squeeze(),
+        #     "attention_mask": encoded_text["attention_mask"].squeeze()
+        # }
 
+        return text
     # Define the preprocessed clinical notes
-    clinical_notes_tokenized = df_dk["clinical_notes"].apply(preprocess_text)
+    clinical_notes_preprocessed= df_dk["clinical_notes"].apply(preprocess_text)
+
+    # Convert to list
+    clinical_notes_list = clinical_notes_preprocessed.tolist()  
 
     # Split the data into training and test sets
-    train_texts, val_texts = train_test_split(clinical_notes_tokenized, test_size=0.2, random_state=42)
+    train_texts, val_texts = train_test_split(clinical_notes_list, test_size=0.2, random_state=42)
+    
+    # Create a Dataset from the dictionary
+    train_dataset = Dataset.from_dict({"text": train_texts})
+    #print(train_dataset);exit()
+    val_dataset = Dataset.from_dict({"text": val_texts})
 
-    # Create a dataset class for our data
-    class ClinicalNotesDataset(Dataset):
-        def __init__(self, texts):
-            self.texts = texts
+    train_df_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    # for batch in train_df_loader:
+    #     print(batch)
+    val_df_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
-        def __len__(self):
-            return len(self.texts)
-
-        def __getitem__(self, idx):
-            text = self.texts.iloc[idx]
-            return {
-                "input_ids": text['input_ids'].squeeze(),
-                "attention_mask": text['attention_mask'].squeeze(),
-                "labels": text['input_ids'].squeeze()  # Use input_ids as labels for causal language modeling
-            }
-
-    # Create data loaders for training and validation
-    train_dataset = ClinicalNotesDataset(train_texts)
-    val_dataset = ClinicalNotesDataset(val_texts)
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
-    # Load the LLaMA model
-    model = LlamaForCausalLM.from_pretrained(llm_model)
-
-    # Ensure the model is on CPU
-    device = torch.device("cpu")
-    model.to(device)
 
     # Define training arguments
     training_args = TrainingArguments(
         output_dir="./results",
+        use_cpu=True,
         num_train_epochs=num_epochs,
-        per_device_train_batch_size=batch_size,
-        learning_rate=1e-5,
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=1,
+        optim="adamw_torch",
+        learning_rate=5e-5,
         weight_decay=0.01,
         logging_dir="./logs",
-        logging_steps=100,
-        save_steps=10000,
-        save_total_limit=2,
+        logging_steps=10,
+        save_steps=0,
+        fp16=False,  # Unable mixed precision training
+        bf16=False,  # Disable bfloat16
+        max_grad_norm=0.3,
+        max_steps=-1,
+        warmup_ratio=0.03,
+        group_by_length=True,
+        lr_scheduler_type="constant",
+        gradient_checkpointing=True,
     )
 
-    # Initialize optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=training_args.learning_rate)
+    torch.cuda.empty_cache()
 
-    # Training loop
-    model.train()
+    # Set supervised fine-tuning parameters
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        compute_metrics=None,
+        train_dataset=train_dataset,
+        dataset_text_field="text",
+        )
+
+    
+    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+
     for epoch in range(num_epochs):
-        total_loss = 0
-        for batch in tqdm(train_loader, desc=f"Epoch {epoch+1} Training", leave=False):
-            input_ids = batch["input_ids"].to(device)
-            attention_mask = batch["attention_mask"].to(device)
-            labels = batch["labels"].to(device)
-            optimizer.zero_grad()
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        model.train()
+        for batch in train_df_loader:
+            inputs = tokenizer(batch["text"], return_tensors="pt", padding=True, truncation=True, max_length=512)
+            inputs = {key: val.to(device) for key, val in inputs.items()}
+            outputs = model(**inputs, labels=inputs["input_ids"])
             loss = outputs.loss
             loss.backward()
             optimizer.step()
-            total_loss += loss.item()
-        print(f"Epoch {epoch+1}, Loss: {total_loss / len(train_loader)}")
+            optimizer.zero_grad()
+            torch.cuda.empty_cache()
+    # Train model
+    # trainer.train()
+
+    # Save trained model
+    trainer.model.save_pretrained(new_model)
 
     
 if __name__ == "__main__":
