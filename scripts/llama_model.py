fcc3a36c848f41c401f86c16e0eab36d216d69da
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-07-23 09:26:17 +0200

    Configuring prompts and saving results

--- scripts/llama_model.py
+++ scripts/llama_model.py
@@ -1,44 +1,21 @@
 """
 Test Llama3 for feature extraction
 """
+import sys
+import time
 import ollama
-from typing import List
+import random
+from tqdm import tqdm
 from pandas import DataFrame
+from typing import List, Tuple, Any
 
 
-def model(df_eng: DataFrame, keyword:str)-> List[str]:
-    # Pull model
-    try:
-        ollama.pull("llama3")
-        print("Model has been pulled")
-    except Exception as error:
-        print("Error", error)
 
-    # Create an empty list
-    cli_notes_list = []
-    # Define the clinical notes
-    clinical_notes = df_eng["clinical_notes"]
-    # Append notes to a list
-    for notes in clinical_notes:
-        cli_notes_list.append(notes)
-    
-    # Define the message for the model
-    message = [
-    {"role": "system", "content": "You are an excellent Danish physician in extracting relevant words from clinical notes"},
-    {"role": "user", "content": "Given the following clinical notes:\n\n".join(cli_notes_list) + "\n\nFind clinically relevant words from the clinical list that are related to the keyword: " + keyword}
-    ]
-    
-    # Response based on model and message
-    try:
-        response = ollama.chat(model='llama3', messages=message, stream=True)
-        return response['message']['content']
-    # Capture an error  
-    except Exception as error:
-        print("Error", error)
-        return "Error"
-    
+# Reconfigure the standard output stream to use UTF-8 encoding.
+sys.stdout.reconfigure(encoding="utf-8")
 
-def model_danish(df_dk: DataFrame, keyword: str)-> List[str]:
+
+def model_danish(df_dk: DataFrame, keyword: str)-> List[Tuple[str]]:
     pass
     # Pull model
     try:
@@ -47,29 +24,48 @@ def model_danish(df_dk: DataFrame, keyword: str)-> List[str]:
     except Exception as error:
         print("Error", error)
 
-    # Create an empty list
-    cli_notes_list = []
     # Define the clinical notes
     clinical_notes = df_dk["clinical_notes"]
-    # Append notes to a list
-    for notes in clinical_notes:
-        cli_notes_list.append(notes)
+
+    # Define batch size and create batches
+    batch_size = 5
+    batches = [clinical_notes[i:i + batch_size] for i in range(0, len(clinical_notes), batch_size)]
+
+    # Create a list to store the response of the model
+    response_list = []
+    # Create an empty list to preprocess the symptoms of patients from the response
+    # diabetes_patients_symp = []
     
-    # Define the message for the model
-    message = [
-    {"role": "system", "content": "You are an excellent Danish physician in extracting relevant words from clinical notes"},
-    {"role": "user", "content": "Given the following clinical notes:\n\n".join(cli_notes_list) + "\n\nFind clinically relevant words from the clinical list that are related to the keyword: " + keyword}
-    ]
-    
-    # Response based on model and message
-    try:
-        response_danish = ollama.chat(model='llama3', messages=message)
-        return response_danish['message']['content']
-    # Capture an error  
-    except Exception as error:
-        print("Error", error)
-        return "Error"
+    for batch in tqdm(batches, desc="Processing batches"):
+        # Construct the message for the current batch
+        messages = [
+            {
+                "role": "user",
+                "content": (
+                    f"Given the following clinical notes, please identify the patients diagnosed with {keyword} "
+                    f"in the text. Find their symptoms and write only the symptoms using comma (e.g Patient Index 1: Symptom1, symptom2 ..., symptom) else if the patient in: {batch} do not has {keyword}, write the patient index and None and the disease and nothing else about them (e.g Patient Index 1: None):\n" + "\n\n".join(batch)
+                )
+            }
+        ]
+
+        try:
+            # Define the response of the model
+            response = ollama.chat(model='llama3', messages=messages)
+            # Define the response of the moder
+            response_content = response["message"]["content"]
+            print(response_content)
+            response_list.append(response_content)
+            # Save the response list to a file
+            with open('response_list.txt', 'w', encoding='utf-8') as file:
+                for response in response_list:
+                    file.write(response + '\n\n')
+
+            # Wait before use the chat
+            time.sleep(random.randint(1, 3))
+        # Create an exception if error occured during response
+        except Exception as error:
+            print(f"{error}")
 
 
 if __name__ == "__main__":
-    model()
+    model_danish()
