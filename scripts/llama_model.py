d5155aacb7a62acc11d2b5b060647e119291c60f
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-07-23 09:42:53 +0200

    Cleaned and documented script

--- scripts/llama_model.py
+++ scripts/llama_model.py
@@ -1,5 +1,11 @@
 """
-Test Llama3 for feature extraction
+Test Llama3 in prompt that extracts symptoms based on keyword in
+Danish clinical notes
+Steps:  1) Pulling the model
+        2) Create batches of clinical notes
+        3) Prompt Engineering
+        4) Ollama chat
+        5) Save results
 """
 import sys
 import time
@@ -7,16 +13,17 @@ import ollama
 import random
 from tqdm import tqdm
 from pandas import DataFrame
-from typing import List, Tuple, Any
-
+from typing import List, Tuple
 
 
 # Reconfigure the standard output stream to use UTF-8 encoding.
 sys.stdout.reconfigure(encoding="utf-8")
 
 
-def model_danish(df_dk: DataFrame, keyword: str)-> List[Tuple[str]]:
-    pass
+def model_danish(df_dk: DataFrame, keyword: str, batch_size: int)-> List[Tuple[str]]:
+    """
+    Extract symptoms from danish clinical notes using Ollama library 
+    """
     # Pull model
     try:
         ollama.pull("llama3")
@@ -27,15 +34,13 @@ def model_danish(df_dk: DataFrame, keyword: str)-> List[Tuple[str]]:
     # Define the clinical notes
     clinical_notes = df_dk["clinical_notes"]
 
-    # Define batch size and create batches
-    batch_size = 5
+    # Split the clinical_notes DataFrame into batches of size batch_size.
     batches = [clinical_notes[i:i + batch_size] for i in range(0, len(clinical_notes), batch_size)]
 
     # Create a list to store the response of the model
     response_list = []
-    # Create an empty list to preprocess the symptoms of patients from the response
-    # diabetes_patients_symp = []
-    
+
+    # Iterate over batches
     for batch in tqdm(batches, desc="Processing batches"):
         # Construct the message for the current batch
         messages = [
@@ -53,14 +58,14 @@ def model_danish(df_dk: DataFrame, keyword: str)-> List[Tuple[str]]:
             response = ollama.chat(model='llama3', messages=messages)
             # Define the response of the moder
             response_content = response["message"]["content"]
-            print(response_content)
+            # Append to the list
             response_list.append(response_content)
             # Save the response list to a file
             with open('response_list.txt', 'w', encoding='utf-8') as file:
                 for response in response_list:
                     file.write(response + '\n\n')
 
-            # Wait before use the chat
+            # Wait before use the chat again
             time.sleep(random.randint(1, 3))
         # Create an exception if error occured during response
         except Exception as error:
