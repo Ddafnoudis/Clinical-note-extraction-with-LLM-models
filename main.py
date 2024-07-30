a6e562094c423068e8021adc66ecca2a89b8f1ed
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2024-07-30 19:46:36 +0200

    Adding comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Testing Llama3 model for extraction of relevant words
+A pipeline for LLama-3.1-8B models training in Danish clinical text data
 """
 # Import libraries and modules
 from pathlib import Path
@@ -8,9 +8,6 @@ from scripts.tokize_input import tokenize_input
 from scripts.param_config import param_configuration
 from scripts.preprocessing_text import preprocessing_text
 from scripts.tokenizer_preprocess import preprocess_tokenizer
-
-# Define keyword and paths
-# KEYWORD = input("Enter the word that you are interested in: ")
 
 # Define the path of the packages
 DF_DK = Path("~/Desktop/Clinical-note-extraction-with-LLM-models/dataset_notes/clin_note_danish_df.tsv").expanduser()
@@ -22,15 +19,14 @@ PARAMS = "llama3_1/Meta-Llama-3.1-8B-Instruct/params.json"
 
 
 def main():
-    """
-    """
     # Configure the model
     tokenizer_model, model, params_config = model_config(tokenizer=TOKENIZER, model=LLM_MODEL, params=PARAMS)
     dim, n_layers, n_heads, n_kv_heads, vocab_size, multiple_of, ffn_dim_multiplier, norm_eps, rope_theta = param_configuration(config=params_config)
     tokenizer = preprocess_tokenizer(tokenizer_model=tokenizer_model)
     # Preprocess the clinical text data
     train_text, test_text = preprocessing_text(df_danish_path=DF_DK)
-    tokenize_input(tokenizer=tokenizer, train_text=train_text)
+    # Tokenize the tokens
+    tokens = tokenize_input(tokenizer=tokenizer, train_text=train_text)
 
 
 if __name__=="__main__":
