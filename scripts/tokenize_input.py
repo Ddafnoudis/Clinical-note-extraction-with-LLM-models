ae371b6b13b089b49dbdf7ec99b93147c0ba72db
Dimitrios Dafnoudis <qmk568@ku.dk>
2024-08-06 12:43:10 +0200

    allowed special characters

---
+++ scripts/tokenize_input.py
@@ -0,0 +1,26 @@
+import torch
+import tiktoken
+from typing import List
+
+
+def tokenize_input(tokenizer: tiktoken.core.Encoding, train_text: List[str])-> torch.Tensor:
+    """
+    From the text encode tokens and return a tensor
+    """
+    # Iterate over the train text
+    for text in train_text:
+        # Enocde the train text and first use a special token. Allow the "[MASK]" special character 
+        tokens = [128000] + tokenizer.encode(text, allowed_special={'[MASK]'})
+        # Convert the list of tokens into a PyTorch tensor
+        tokens = torch.tensor(tokens)
+        # Decode each token back into its corresponding string for you to see
+        prompt_split_token = [tokenizer.decode([token.item()]) for token in tokens]
+        # print(tokens)
+        # print(prompt_split_token)
+        # exit()
+
+    return tokens
+
+
+if __name__=="__main__":
+    tokenize_input()
